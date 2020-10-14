e5802e6c7eadb3190e3b3b036f8aa300d4363790
Alex Irlbeck <irlbeck1@iastate.edu>
2020-10-14 18:31:06 -0500

    Random file I use for manual testing. Gets overwritten oftern.

---
+++ DomainProject/src/test/java/newspaper/Alex_Maunual_Testing.java
@@ -0,0 +1,12 @@
+package newspaper;
+
+public class Alex_Maunual_Testing<E> {
+	public static void main(String args[])
+	{
+        ArticleManager aman = new ArticleManager();
+	}
+	public void Print(E item)
+	{
+		System.out.println(item);
+	}
+}
