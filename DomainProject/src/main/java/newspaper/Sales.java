9399e06b1928a2346b1bc53ab2189c41ed09ac48
Alex Irlbeck <irlbeck1@iastate.edu>
2020-10-14 15:24:42 -0500

    Some Javadoc added.

--- DomainProject/src/main/java/newspaper/Sales.java
+++ DomainProject/src/main/java/newspaper/Sales.java
@@ -1,5 +1,10 @@
 package newspaper;
 
+/**
+ * A simple class that basically returns true no matter what method is called.
+ * @author Alexander Irlbeck
+ *
+ */
 public class Sales {
 	public boolean cardSale(String cardNum, int cvv, int expiration, String Name_on_card)
 	{
