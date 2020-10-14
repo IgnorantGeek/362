f4019168a8777d9e01dcc625048bef18cc69c28f
Alex Irlbeck <irlbeck1@iastate.edu>
2020-10-14 16:12:05 -0500

    Converted to use Strings, since it really doesn't matter.

--- DomainProject/src/main/java/newspaper/Sales.java
+++ DomainProject/src/main/java/newspaper/Sales.java
@@ -6,7 +6,7 @@ package newspaper;
  *
  */
 public class Sales {
-	public boolean cardSale(String cardNum, int cvv, int expiration, String Name_on_card)
+	public boolean cardSale(String cardNum, String cvv, String expiration, String Name_on_card)
 	{
 		return true;
 	}
