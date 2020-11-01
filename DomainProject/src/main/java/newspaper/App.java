2f333cec9561a424417125958b0953c07b66f267
Nick Heisler <nickheisler63@gmail.com>
2020-11-01 16:44:59 -0600

    working on employee addition/remove

--- DomainProject/src/main/java/newspaper/App.java
+++ DomainProject/src/main/java/newspaper/App.java
@@ -400,11 +400,13 @@ public class App
 									System.out.println("Failed to remove Distributor with ID: " + input +" no entry found.");
 								}
 								else System.out.println("Successfully remove Distributor: " + input);
+								break;
+							default:
+								System.out.println("Seriously?");
 						}
 
 						break;
 					case "6":
-					{
 						System.out.println("Welcome to our reviews section. Would you like to see all of our reviews (enter 1), enter a new review (enter 2), or");
 						System.out.println("delete a review (enter 3).");
 						input = in.nextLine();
@@ -412,20 +414,80 @@ public class App
 						switch(input)
 						{
 							case "1":
-							{
+							
 								feedback.displayFeedback();
-							}
+								break;
 							case "2":
-							{
 								feedback.giveFeedback();
-							}
+								break;
 							case "3":
-							{
 								feedback.removeFeedback(clearance);
-							}
+								break;
+							default:
+								System.out.println("Not a valid option. Exiting.");
 						}
 						break;
-					}
+					case "7":
+						// Add or remove an employee
+						System.out.println("Would you like to add or remove an employee? (a/r)");
+
+						input = in.nextLine();
+
+						switch (input)
+						{
+							case "a":
+							case "add":
+							case "ADD":
+							case "Add":
+								// Add an employee
+								System.out.println("Please enter the full name of the new employee:");
+								String name = in.nextLine();
+								
+								System.out.println("Are you the supervisor for this employee? (y/n)");
+								input = in.nextLine();
+
+								if (input.compareTo("y") == 0)
+								{
+									while (true)
+									{
+										System.out.println("Please enter the password for the new user:");
+										String newPassword = in.nextLine();
+
+										System.out.println("Please re-enter your password:");
+										input = in.nextLine();
+
+										if (newPassword.compareTo(input) == 0)
+										{
+											break;
+										}
+										else
+										{
+											System.out.println("The passwords you entered did not match!\n");
+										}
+									}
+								}
+								else if (input.compareTo("n") == 0)
+								{
+
+								}
+								else
+								{
+									System.out.println("You had the option to enter a 'y' or an 'n'. You chose to answer '" +
+									input +"'. Why are you like this?");
+								}
+								
+								break;
+							case "r":
+							case "remove":
+							case "Remove":
+							case "REMOVE":
+								// Remove an employee
+								break;
+							default:
+								break;
+						}
+
+						break;
 					case "q":
 					case "quit":
 					case "Quit":
@@ -434,11 +496,14 @@ public class App
 						Global.flushConsole();
 						loggedIn = null;
 						continue outer;
+					default:
+						System.out.println("No binding for input: " + input);
 					}
 
 					Global.flushConsole();
 					System.out.println("Now what would you like to do?");
-					System.out.println("1: Edit/Publish a Newspaper\n2: Edit/Create an Article\n3: Enter a New Ad Sale\n4: Add/Remove a Subscription\n5: Add/Remove a Distributor\nq: Quit");
+					System.out.println("1: Edit/Publish/View a Newspaper\n2: Edit/Create/View an Article\n3: Enter a New Ad Sale" +
+					"\n4: Add/Remove a Subscription\n5: Add/Remove a Distributor\n6: To see our reviews\n7: Add/Remove an Employee\nq: Logout");
 				}
 			}
 			else
--- DomainProject/src/main/java/newspaper/managers/EmployeeManager.java
+++ DomainProject/src/main/java/newspaper/managers/EmployeeManager.java
@@ -20,7 +20,7 @@ public class EmployeeManager
     // Constructors
     public EmployeeManager()
     {
-        registry = new HashMap<Integer, Employee>();
+        registry = new HashMap<>();
         idCounter = 0;
         employeeCount = 0;
         this.init();
@@ -30,7 +30,7 @@ public class EmployeeManager
     {
         this.idCounter = start;
         employeeCount = 0;
-        registry = new HashMap<Integer, Employee>();
+        registry = new HashMap<>();
         this.init();
     }
 
@@ -50,8 +50,7 @@ public class EmployeeManager
 
         registry.put(in.Id(), in);
 
-        if (in.write() < 0) return false;
-        return true;
+        return (in.write() > 0) ? true : false;
     }
 
     /**
@@ -111,7 +110,7 @@ public class EmployeeManager
      * Employee files into memory
      * @return true on success, false otherwise
      */
-    public boolean init()
+    public void init()
     {
         // Check for presence of Employee DB entry, build if not present
         File rootDir = new File(Global.EMPLOYEE_DB_PATH);
@@ -119,7 +118,7 @@ public class EmployeeManager
         if (!rootDir.exists())
         {
             rootDir.mkdir();
-            return true;
+            return;
         }
 
         for (String employeeFileName : rootDir.list())
@@ -135,19 +134,17 @@ public class EmployeeManager
             // Prevents us from overwriting employee files that failed to add
             idCounter++;
         }
-
-        return true;
     }
 
     /**
      * Builds an Employee class from a filename
-     * @param Filename - Name of file (relative path)
+     * @param fileName - Name of file (relative path)
      * @return New Employee class, null if build failed
      */
-    public Employee buildFromFile(String Filename)
+    public Employee buildFromFile(String fileName)
     {
         Employee out = null;
-        File employeeFile = new File(Global.EMPLOYEE_DB_PATH + Filename);
+        File employeeFile = new File(Global.EMPLOYEE_DB_PATH + fileName);
         Scanner fileScanner;
 
         try 
@@ -159,10 +156,10 @@ public class EmployeeManager
             String password = fileScanner.nextLine();
 
             String fname = "";
-            for (int i = 0; i < Filename.length(); i++)
+            for (int i = 0; i < fileName.length(); i++)
             {
-                if (Filename.charAt(i) == '.') break;
-                else fname += Filename.charAt(i);
+                if (fileName.charAt(i) == '.') break;
+                else fname += fileName.charAt(i);
             }
             int id = Integer.parseInt(fname);
 
@@ -172,7 +169,7 @@ public class EmployeeManager
         }
         catch (Exception e)
         {
-            System.out.println("ERROR: Failed to add employee file: " + Filename);
+            System.out.println("ERROR: Failed to add employee file: " + fileName);
             System.out.println(e.getMessage());
             return null;
         }
